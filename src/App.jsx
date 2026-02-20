2f18519995482af58b876558a6af967d313d5055
aashildf <faas0825@gmail.com>
2026-02-20 14:24:05 +0100

    header og hero

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,16 @@ import ApiCarousel from "./components/ApiCarousel/ApiCarousel";
 import Cases from "./components/Cases";
 import About from "./components/About";
 import Footer from "./components/Footer";
+import IntroSection from "./components/IntroSection";
 
 export default function App() {
   return (
     <>
-      <Header />
-      <HeroText />
+      <div className="texture-wrapper">
+        <Header />
+        <HeroText/>
+      </div>
+      <IntroSection />
       <ApiCarousel />
       <Cases />
       <About />
---
+++ src/components/IntroSection.jsx
@@ -0,0 +1,8 @@
+export default function IntroSection() {
+  return (
+    <section className="intro-section">
+      <h2>Hei her kommer innholdet som skal stå i seksjonen, hallo, hallo</h2>
+      <p>Og her kommer mer tekst...</p>
+    </section>
+  );
+}
